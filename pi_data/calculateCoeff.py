743781acb87a13d22395f98fdec21c6cc72ea6d8
muke101 <muke101@protonmail.com>
2019-11-16 20:48:50 +0000

    extended time peroids over day for traffic

--- pi_data/calculateCoeff.py
+++ pi_data/calculateCoeff.py
@@ -5,16 +5,23 @@ import popdensity.popdensity as pd
 class calCoeff:
     def __init__(self):
         coeffs = []
+        self.notifs = {'raining': False, 'icy': False}
 
     def calWeather(self, k=0.05):
         data = wt.pullData()
         averageTemp = 15
         subCoeff = abs(averageTemp-data['Air_temperature'])*k+data['Relative_humidity']/data['Pressure']
-        isRaining = True if abs(data['Dew_Point']-data['Air_temperature']) < 1 else False 
-        return subCoeff, isRaining
+        if abs(data['Dew_Point']-data['Air_temperature']) < 1:
+            self.notifs['raining'] = True
+        return subCoeff
 
     def calTraffic(self):
         return tr.countTraffic
-        
-            
-        
+
+    def calPopDensity(self):
+        std = 4898
+        mean = 85
+        threshhold = 6000
+         
+
+
