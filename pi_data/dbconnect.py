bd3691f1925999a7c470497e721ea88dc02e271c
muke101 <muke101@protonmail.com>
2019-11-16 13:40:04 +0000

    added database connection script

---
+++ pi_data/dbconnect.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+import os
+
+def dbConnect(
+            host=os.environ['POSTGRES_HOST'],
+            db=os.environ['POSTGRES_DB'],
+            user=os.environ['POSTGRES_USER'],
+            password=os.environ['POSTGRES_PASSWORD'],
+            port=5432
+            ):
+    
+    dbConnectionString = 'postgressql://+'user+':'+password+'@'+host+'/'+db+'/'port
+    
+    engine = create_engine(dbConnectionString)
+    
+    return engine
+
--- pi_data/traffic.py
+++ pi_data/traffic.py
@@ -3,4 +3,20 @@
 #directly proportional to sub-coeff
 #could possibly calculate density as well using ranges given rather than direct numbers
 #at certain threshholds can send alerts like 'possible event happening here'
+from sqlalchemy import create_engine
+import os
+
+host = os.environ['POSTGRES_HOST']
+db = os.environ['POSTGRES_DB']
+user = os.environ['POSTGRES_USER']
+password = os.environ['POSTGRES_PASSWORD']
+port = 5432
+dbConnectionString = 'postgressql://+'user+':'+password+'@'+host+'/'+db+'/'port
+
+engine = create_engine(dbConnectionString)
+
+#SQL query goes here
+
+totalNumber = 20
+
 
