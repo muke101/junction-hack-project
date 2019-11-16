57d0e8a49885d9b99d2b336550c744f32c06597a
iita <idamaria.lehtinen@iki.fi>
2019-11-16 21:28:11 +0200

    sqlAlchemy query, untested

--- pi_data/weather.py
+++ pi_data/weather.py
@@ -1,12 +1,23 @@
 import dbconnect as db
 
+
+
 class weather:
     def __init__(self):
         self.engine, self.bf15 = db.dbConnect()
         self.conn = self.engine.connect()
 
-    def pullData(self): 
-        
-        dbPull = 
+    def pullData(self, input_time): 
 
-        return dbPull
+        HW = sa.Table(
+            'Helsinki_Weather',
+            metadata,
+            autoload=True,
+            autoload_with=engine
+        ) 
+
+        sel = sa.select([HW.c.Air_temperature, HW.c.Relative_humidity, HW.c.Pressure, HW.c.Dewpoint, HW.c.timestamp]
+        ).where(HW.c.timestamp==input_time)
+
+        dbPull=self.conn.execute(sel).fetchall()
+        return(dbPull)
