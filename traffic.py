db6d7cf192aa518664fbabb4b0234f06b8f745ea
Antti Vikman <antti.vikman@nolwenture.com>
2019-11-16 21:05:39 +0200

    Fixed the multithreading, will not generate proper JSON and likely should just write this to DB anyway

--- traffic.py
+++ traffic.py
@@ -1,10 +1,11 @@
 import requests
-from multiprocessing import Pool
+from multiprocessing import Pool, Lock
 import pandas as pd
+
 def query(time_interval):
     url = "https://api.hypr.cl/raw/"
     headers = {
-     'x-api-key': "**",
+     'x-api-key': "iQ0WKQlv3a7VqVSKG6BlE9IQ88bUYQws6UZLRs1B",
      'time_start': "2019-08-01T"+time_interval[0], 'time_stop': "2019-08-01T"+time_interval[1], 'Accept': "*/*",
      'Cache-Control': "no-cache", 'Host': "api.hypr.cl",
      'Accept-Encoding': "gzip, deflate",
@@ -12,27 +13,30 @@ def query(time_interval):
      'cache-control': "no-cache" }
     response = requests.request("POST", url, headers=headers)
     #responsedata = pd.DataFrame(response.text)
-    print(response.text)
-    return response.text  
+    #print(response.text)
+    with lock:
+        file.write('"' + time_interval[0] + '":' + response.text + ',')
+        print('Processed: ' + str(time_interval))
 
-p = Pool(16)
-f = open('myfile3.txt', 'a')
-f.write(p.map(query, [('12:00:00Z','12:00:01Z'),
-                    ('12:00:01Z','12:00:02Z'),
-                    ('12:00:02Z','12:00:03Z'),
-                    ('12:00:03Z','12:00:04Z'),
-                    ('12:00:04Z','12:00:05Z'),
-                    ('12:00:05Z','12:00:06Z'),
-                    ('12:00:06Z','12:00:07Z'),
-                    ('12:00:07Z','12:00:08Z'),
-                    ('12:00:08Z','12:00:09Z'),
-                    ('12:00:09Z','12:00:10Z'),
-                    ('12:00:10Z','12:00:11Z'),
-                    ('12:00:11Z','12:00:12Z'),
-                    ('12:00:12Z','12:00:13Z'),
-                    ('12:00:13Z','12:00:14Z'),
-                    ('12:00:14Z','12:00:15Z'),
-                    ('12:00:15Z','12:00:16Z')])
-f.close()
+
+def init_child(lock_, file_):
+    global lock
+    lock = lock_
+    global file
+    file = file_
+
+def main():
+    lock = Lock()
+    file = open('gotValues.json', 'a')
+    file.truncate(0)
+    file.write('{')
+    with Pool(59, initializer=init_child, initargs=(lock, file)) as pool:
+        times = list(map(lambda x: ('12:'+ str(x).zfill(2) +':00Z','12:'+ str(x).zfill(2) +':59Z'), range(59)))
+        pool.map(query, times)
+    file.write('}')
+    file.close()
+
+if __name__ == "__main__":
+    main()
 
 
