70a62c13bef2fded4a00342e99f0b2e3d0504e5f
muke101 <muke101@protonmail.com>
2019-11-16 12:02:54 +0000

    added template for traffic recording script

---
+++ pi_data/traffic.py
@@ -0,0 +1,6 @@
+#function to pull data that gets called periodically
+#counts total number of detections over peroid
+#directly proportional to sub-coeff
+#could possibly calculate density as well using ranges given rather than direct numbers
+#at certain threshholds can send alerts like 'possible event happening here'
+
