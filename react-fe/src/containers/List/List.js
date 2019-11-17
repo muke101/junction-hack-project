f57d68805c48e6ed7518bc8cf0154315058e166b
lvlr <lvlr@obsidianforge.org>
2019-11-17 09:52:21 +0200

    Things are less ugly?

--- react-fe/src/containers/List/List.js
+++ react-fe/src/containers/List/List.js
@@ -2,13 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ListContainer = styled.div`
-  background-color: ${props => props.backgroundColor};
-  grid-area: list;
+/*  background-color: ${props => props.backgroundColor};
+  grid-area: list;*/
+  padding-top: 0.5rem;
 `;
 
 export function List(props) {
   return (
-    <ListContainer backgroundColor={props.backgroundColor}>
+    <ListContainer palette={props.palette}>
       {props.children}
     </ListContainer>
   );
